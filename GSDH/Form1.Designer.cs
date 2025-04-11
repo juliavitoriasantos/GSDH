9ed856a5e7b8e93a1fc3b620bec1c1053bd41d32
juliavitoriasantos <cruz.vitoria@aluno.ifsp.edu.br>
2025-04-11 17:00:36 -0300

    Adicionar arquivos de projeto.

---
+++ GSDH/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace GSDH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.loginsDataSet = new GSDH.LoginsDataSet();
+            this.loginsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.loginsTableAdapter = new GSDH.LoginsDataSetTableAdapters.LoginsTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usernameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.passwordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.lblMensagem3 = new System.Windows.Forms.Label();
+            this.lblMensagem2 = new System.Windows.Forms.Label();
+            this.txtUsuarioLogin = new System.Windows.Forms.TextBox();
+            this.txtUsuarioSenha = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.lblConfirmar = new System.Windows.Forms.Label();
+            this.lblSenhaR = new System.Windows.Forms.Label();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtUsuarioR = new System.Windows.Forms.TextBox();
+            this.txtConfirmar = new System.Windows.Forms.TextBox();
+            this.txtSenhaR = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loginsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loginsBindingSource)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(47, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(520, 372);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnLogin);
+            this.tabPage1.Controls.Add(this.txtUsuarioSenha);
+            this.tabPage1.Controls.Add(this.txtUsuarioLogin);
+            this.tabPage1.Controls.Add(this.lblMensagem2);
+            this.tabPage1.Controls.Add(this.lblMensagem3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(512, 346);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Login";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(512, 346);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Banco de Dados";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.usernameDataGridViewTextBoxColumn,
+            this.passwordDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.loginsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(343, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // loginsDataSet
+            // 
+            this.loginsDataSet.DataSetName = "LoginsDataSet";
+            this.loginsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // loginsBindingSource
+            // 
+            this.loginsBindingSource.DataMember = "Logins";
+            this.loginsBindingSource.DataSource = this.loginsDataSet;
+            // 
+            // loginsTableAdapter
+            // 
+            this.loginsTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // usernameDataGridViewTextBoxColumn
+            // 
+            this.usernameDataGridViewTextBoxColumn.DataPropertyName = "Username";
+            this.usernameDataGridViewTextBoxColumn.HeaderText = "Username";
+            this.usernameDataGridViewTextBoxColumn.Name = "usernameDataGridViewTextBoxColumn";
+            // 
+            // passwordDataGridViewTextBoxColumn
+            // 
+            this.passwordDataGridViewTextBoxColumn.DataPropertyName = "Password";
+            this.passwordDataGridViewTextBoxColumn.HeaderText = "Password";
+            this.passwordDataGridViewTextBoxColumn.Name = "passwordDataGridViewTextBoxColumn";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.txtEmail);
+            this.tabPage3.Controls.Add(this.txtSenhaR);
+            this.tabPage3.Controls.Add(this.txtConfirmar);
+            this.tabPage3.Controls.Add(this.txtUsuarioR);
+            this.tabPage3.Controls.Add(this.lblEmail);
+            this.tabPage3.Controls.Add(this.lblMensagem);
+            this.tabPage3.Controls.Add(this.lblSenhaR);
+            this.tabPage3.Controls.Add(this.lblConfirmar);
+            this.tabPage3.Controls.Add(this.btnRegistro);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(512, 346);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Registro";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // lblMensagem3
+            // 
+            this.lblMensagem3.AutoSize = true;
+            this.lblMensagem3.Location = new System.Drawing.Point(55, 20);
+            this.lblMensagem3.Name = "lblMensagem3";
+            this.lblMensagem3.Size = new System.Drawing.Size(43, 13);
+            this.lblMensagem3.TabIndex = 0;
+            this.lblMensagem3.Text = "Usuário";
+            // 
+            // lblMensagem2
+            // 
+            this.lblMensagem2.AutoSize = true;
+            this.lblMensagem2.Location = new System.Drawing.Point(55, 112);
+            this.lblMensagem2.Name = "lblMensagem2";
+            this.lblMensagem2.Size = new System.Drawing.Size(38, 13);
+            this.lblMensagem2.TabIndex = 1;
+            this.lblMensagem2.Text = "Senha";
+            // 
+            // txtUsuarioLogin
+            // 
+            this.txtUsuarioLogin.Location = new System.Drawing.Point(216, 20);
+            this.txtUsuarioLogin.Name = "txtUsuarioLogin";
+            this.txtUsuarioLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuarioLogin.TabIndex = 2;
+            // 
+            // txtUsuarioSenha
+            // 
+            this.txtUsuarioSenha.Location = new System.Drawing.Point(216, 109);
+            this.txtUsuarioSenha.Name = "txtUsuarioSenha";
+            this.txtUsuarioSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuarioSenha.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(216, 230);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Location = new System.Drawing.Point(218, 247);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistro.TabIndex = 0;
+            this.btnRegistro.Text = "Registro";
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            this.btnRegistro.Click += new System.EventHandler(this.btnRegistro_Click);
+            // 
+            // lblConfirmar
+            // 
+            this.lblConfirmar.AutoSize = true;
+            this.lblConfirmar.Location = new System.Drawing.Point(96, 153);
+            this.lblConfirmar.Name = "lblConfirmar";
+            this.lblConfirmar.Size = new System.Drawing.Size(51, 13);
+            this.lblConfirmar.TabIndex = 1;
+            this.lblConfirmar.Text = "Confirmar";
+            // 
+            // lblSenhaR
+            // 
+            this.lblSenhaR.AutoSize = true;
+            this.lblSenhaR.Location = new System.Drawing.Point(96, 98);
+            this.lblSenhaR.Name = "lblSenhaR";
+            this.lblSenhaR.Size = new System.Drawing.Size(38, 13);
+            this.lblSenhaR.TabIndex = 2;
+            this.lblSenhaR.Text = "Senha";
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Location = new System.Drawing.Point(96, 42);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(43, 13);
+            this.lblMensagem.TabIndex = 3;
+            this.lblMensagem.Text = "Usuário";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(96, 214);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtUsuarioR
+            // 
+            this.txtUsuarioR.Location = new System.Drawing.Point(193, 42);
+            this.txtUsuarioR.Name = "txtUsuarioR";
+            this.txtUsuarioR.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuarioR.TabIndex = 8;
+            // 
+            // txtConfirmar
+            // 
+            this.txtConfirmar.Location = new System.Drawing.Point(193, 150);
+            this.txtConfirmar.Name = "txtConfirmar";
+            this.txtConfirmar.Size = new System.Drawing.Size(100, 20);
+            this.txtConfirmar.TabIndex = 9;
+            // 
+            // txtSenhaR
+            // 
+            this.txtSenhaR.Location = new System.Drawing.Point(193, 95);
+            this.txtSenhaR.Name = "txtSenhaR";
+            this.txtSenhaR.Size = new System.Drawing.Size(100, 20);
+            this.txtSenhaR.TabIndex = 10;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(193, 207);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loginsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loginsBindingSource)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private LoginsDataSet loginsDataSet;
+        private System.Windows.Forms.BindingSource loginsBindingSource;
+        private LoginsDataSetTableAdapters.LoginsTableAdapter loginsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn usernameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn passwordDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUsuarioSenha;
+        private System.Windows.Forms.TextBox txtUsuarioLogin;
+        private System.Windows.Forms.Label lblMensagem2;
+        private System.Windows.Forms.Label lblMensagem3;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Label lblSenhaR;
+        private System.Windows.Forms.Label lblConfirmar;
+        private System.Windows.Forms.Button btnRegistro;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSenhaR;
+        private System.Windows.Forms.TextBox txtConfirmar;
+        private System.Windows.Forms.TextBox txtUsuarioR;
+    }
+}
+
---
+++ GSDH/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GSD_HASH;
+
+namespace GSDH
+{
+    public partial class Form1 : Form
+    {
+
+        private string connectionString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectionString = "Data Source=10.125.50.191;Initial Catalog=Logins;User ID=aluno;Password=aluno;";
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // Abrindo a conexão
+                    connection.Open();
+                    MessageBox.Show("Conexão com o banco de dados bem-sucedida!");
+
+
+                }
+                catch (Exception ex)
+                {
+                    // Se ocorrer algum erro, mostramos a mensagem de erro
+                    MessageBox.Show("Erro ao conectar ao banco de dados: " + ex.Message);
+                }
+                finally
+                {
+                    // A conexão será fechada automaticamente pelo 'using'
+                }
+            }
+        }
+
+
+
+        private void btnRegistro_Click(object sender, EventArgs e)
+        {
+            AdicionarUsuario(txtUsuarioR.Text, txtUsuarioSenha.Text, txtConfirmar.Text, txtEmail.Text);
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string usuario = txtUsuarioLogin.Text.Trim();
+            string senha = Crypto.sha256encrypt(txtUsuarioSenha.Text.Trim());
+            string connectionString = "Data Source=10.125.50.191;Initial Catalog=Logins;User ID=aluno;Password=aluno;";
+            string query = "SELECT COUNT(*) FROM acessos WHERE usuario = @usuario AND senha = @senha";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@usuario", usuario);
+                cmd.Parameters.AddWithValue("@senha", senha);
+
+                conn.Open();
+
+                int resultado = (int)cmd.ExecuteScalar();
+
+                if (resultado > 0)
+                {
+                    txtUsuarioLogin.Text = String.Empty;
+                    txtUsuarioSenha.Text = String.Empty;
+                    MessageBox.Show("Login realizado com sucesso!");
+                }
+                else
+                {
+                    MessageBox.Show("Usuário/Senha incorretos");
+                }
+            }
+        }
+
+        private void AdicionarUsuario(string _nomeUsuario, string _senha, string _confirmaSenha, string _email)
+        {
+
+            Match match = Regex.Match(_email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+
+            // Confirma a senha
+            if (_senha != _confirmaSenha)
+            {
+                MessageBox.Show("A senha não confere.");
+            }
+            // A senha tem que ter no mínimo 8 caracteres
+            else if (_senha.Length < 8)
+            {
+                MessageBox.Show("A senha deve conter no mínimo 8 caracteres");
+            }
+            // Se o email não for válido
+            else if (!match.Success)
+            {
+                MessageBox.Show("Email inválido");
+            }
+            // Se não informou o usuário
+            else if (_nomeUsuario == null)
+            {
+                MessageBox.Show("Você deve informar um usuário");
+            }
+            // Se estiver tudo certo, então cria o usuário
+            else
+            {
+                // Criptografa a senha
+                string _hashSenha = Crypto.sha256encrypt(_senha);
+
+                AdicionaUsuarioNoBD(_nomeUsuario, _hashSenha, _email);
+
+                // Limpa os campos do formulário
+                txtUsuarioR.Text = String.Empty;
+                txtSenhaR.Text = String.Empty;
+                txtConfirmar.Text = String.Empty;
+                txtEmail.Text = String.Empty;
+
+                MessageBox.Show("Obrigado por seu registro!");
+            }
+        }
+        private void AdicionaUsuarioNoBD(string _nomeUsuario, string _senha, string _email)
+        {
+            using (SqlConnection cn = new SqlConnection(connectionString))  // AQUI você passa a connectionString
+            {
+                try
+                {
+                    // Abrindo a conexão
+                    cn.Open();
+
+                    // Definindo o comando SQL para inserir o usuário
+                    string sql = "INSERT INTO Acessos (usuario, senha, email) VALUES (@usuario, @senha, @email)";
+
+                    // Criando o comando SQL com parâmetros
+                    using (SqlCommand cmd = new SqlCommand(sql, cn))
+                    {
+                        // Adicionando os parâmetros
+                        cmd.Parameters.Add(new SqlParameter("@usuario", SqlDbType.NVarChar)).Value = _nomeUsuario;
+                        cmd.Parameters.Add(new SqlParameter("@senha", SqlDbType.NVarChar)).Value = _senha;
+                        cmd.Parameters.Add(new SqlParameter("@email", SqlDbType.NVarChar)).Value = _email;
+
+                        // Executando o comando no banco de dados
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    // Mensagem de sucesso
+                    MessageBox.Show("Usuário adicionado com sucesso.");
+                }
+                catch (SqlException sqlexception)
+                {
+                    // Tratamento de erro
+                    MessageBox.Show(sqlexception.Message, "Erro ao adicionar usuário", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    // Tratamento de erro genérico
+                    MessageBox.Show(ex.Message, "Erro desconhecido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    // A conexão será fechada automaticamente pelo 'using'
+                }
+
+            }
+
+
+        }
+    }
+
+}
